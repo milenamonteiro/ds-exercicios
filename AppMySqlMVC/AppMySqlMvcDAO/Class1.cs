67e73539f60c43905a423ae6e70bb4d8bac160b6
3A Etim Info <3aetim@basilides.local>
2019-10-18 11:55:51 -0300

    Tia o programa novo

---
+++ AppMySqlMVC/AppMySqlMvcDAO/Class1.cs
@@ -0,0 +1,34 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace AppMySqlMvcDAO
+{
+    public class ConexaoDB
+    {
+        private static ConexaoDB objConexaoDB = null;
+        private readonly MySqlConnection Conexao;
+
+        private ConexaoDB()
+        {
+            Conexao = new MySqlConnection("server=localhost; user id=root; password=1234567; database=dbECOMMERCE");
+        }
+
+        public static ConexaoDB ConexaoStatus()
+        {
+            if(objConexaoDB == null)
+            {
+                objConexaoDB = new ConexaoDB();
+            }
+            return objConexaoDB;
+        }
+
+        public MySqlConnection ConexaoPegar()
+        {
+            return Conexao;
+        }
+
+        public void ConexaoFechar()
+        {
+            objConexaoDB = null;
+        }
+    }
+}
